b25044a201a3445caf5786b0322efb339e192077
Abhishek <abhitiwari3010@gmail.com>
2022-12-27 17:37:09 +0530

    Added comments and removed unnecessary code lines

--- Moment/auth/home_script.js
+++ Moment/auth/home_script.js
@@ -1,4 +1,7 @@
+// Start Exploring the auth folder from auth.html 
 
+
+// Everything is same as we have done before...
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
   import { getDatabase, ref, get,child,onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
  
@@ -11,22 +14,20 @@
     appId: "1:754364220129:web:2a20aca64d6044c3e1eda0",
     
   };
+ initializeApp(firebaseConfig);
 
-  initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  localStorage.clear();
+  
+  const db = getDatabase();
 
-  var uid=JSON.parse(sessionStorage.getItem("id"));
-   const db = getDatabase();
-  var nameOfCurrentUser;
+  // creating a function for getting data from database named as SelectData()
   function SelectData()
   {
     const dbref = ref(db);
-    console.log("users/"+JSON.parse(sessionStorage.getItem("id")));
+  //running get function that we have imported above and getting the snapshot of the user's data
     get(child(dbref, "users/"+JSON.parse(sessionStorage.getItem("id")))).then((snapshot) => {
       if (snapshot.exists()) {
+       //changing the innerHTML of some Elements of home.html equal to corresponding snapshot values
         document.getElementById("nameOfUser").innerHTML=snapshot.val().Name;
         document.getElementById("emailOfUser").innerHTML=snapshot.val().Email;
         document.getElementById("telOfUser").innerHTML=snapshot.val().Telephone;
@@ -38,8 +39,11 @@
     });
     
   }
+// Calling the funtion that we just created....
   SelectData();
-  console.log(uid);
+
+  // That's it auth folder has only these files, 
+  // In these files home.html and home_script.js are created for testing the functionality of the webapp
   
 
   
