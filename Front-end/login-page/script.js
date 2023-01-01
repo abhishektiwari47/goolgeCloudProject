135b30274bce637c9934de54853f3c53dc93150f
Abhishek <abhitiwari3010@gmail.com>
2023-01-01 09:05:55 +0530

    Login and signup on the same page

--- Front-end/login-page/script.js
+++ Front-end/login-page/script.js
@@ -3,4 +3,14 @@ const navbarLinks =  document.getElementsByClassName("navbar-links")[0]
 
 toggleButton.addEventListener('click',()=>{
     navbarLinks.classList.toggle('active');
-})
+});
+
+document.getElementById("have-account").addEventListener('click',function(){
+    document.getElementById("login-div").style.display='inline';
+    document.getElementById("signup-div").style.display="none";
+  });
+  
+  document.getElementById("create-account").addEventListener('click',function(){
+    document.getElementById("signup-div").style.display='inline';
+    document.getElementById("login-div").style.display="none";
+  });
